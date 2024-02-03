24da1908612714dd32e5e85bfac8c2a596a9a1a4
qianmoQ <shicheng@devlive.org>
2024-02-03 13:08:52 +0800

    Add fontawesome support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import {createApp} from "vue";
-import "./styles.css";
-import App from "./App.vue";
-import router from "./router";
+import {createApp} from "vue"
+import "./styles.css"
+import App from "./App.vue"
+import router from "./router"
+import {createIcons} from "@/fontawesome.ts"
 
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
+const app = createApp(App)
+createIcons(app)
+app.use(router)
+app.mount("#app")
